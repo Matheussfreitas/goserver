d017e78256e680fdbed34f60b07b8ac05bea368a
Matheus Freitas <mgsoutodelira@gmail.com>
2025-12-20 13:31:55 -0300

    feat: create env config and jwt

---
+++ internal/auth/jwt.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"goserver/internal/env"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte(env.LoadConfig().JWTSecret)
+
+func GenerateToken(email string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"authorized": true,
+		"email":      email,
+		"exp":        time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	return token.SignedString(jwtKey)
+}
---
+++ internal/plataform/env/env.go
@@ -0,0 +1,20 @@
+package env
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port      string
+	JWTSecret string
+}
+
+func LoadConfig() *Config {
+	godotenv.Load()
+	return &Config{
+		Port:      os.Getenv("PORT"),
+		JWTSecret: os.Getenv("JWT_SECRET"),
+	}
+}
