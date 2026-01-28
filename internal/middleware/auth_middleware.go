528d94d7e1a36f530695f5bfc68efa2d85b37cf5
Matheus Freitas <mgsoutodelira@gmail.com>
2026-01-27 21:04:39 -0300

    feat: Implement user-specific quiz management by propagating user IDs through quiz services and repository methods.

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -18,19 +18,19 @@ func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
+			w.WriteHeader(http.StatusUnauthorized)
 			json.NewEncoder(w).Encode(map[string]string{
 				"message": "Token não fornecido",
 			})
-			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
 
 		parts := strings.Split(authHeader, " ")
 		if len(parts) != 2 || parts[0] != "Bearer" {
+			w.WriteHeader(http.StatusUnauthorized)
 			json.NewEncoder(w).Encode(map[string]string{
 				"message": "Formato de token inválido",
 			})
-			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
 
@@ -38,23 +38,34 @@ func AuthMiddleware(next http.Handler) http.Handler {
 
 		token, err := utils.ValidateToken(tokenString)
 		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
 			json.NewEncoder(w).Encode(map[string]string{
 				"message": "Token inválido ou expirado",
 			})
-			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
 
 		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 			email, ok := claims["email"].(string)
 			if !ok {
+				w.WriteHeader(http.StatusUnauthorized)
 				json.NewEncoder(w).Encode(map[string]string{
 					"message": "Token inválido: email não encontrado",
 				})
+				return
+			}
+
+			id, ok := claims["sub"].(string)
+			if !ok {
 				w.WriteHeader(http.StatusUnauthorized)
+				json.NewEncoder(w).Encode(map[string]string{
+					"message": "Token inválido: ID não encontrado",
+				})
 				return
 			}
 			ctx := context.WithValue(r.Context(), UserEmailKey, email)
+			ctx = context.WithValue(ctx, "user_id", id)
+
 			next.ServeHTTP(w, r.WithContext(ctx))
 		}
 	})
